16da1f04a0f92106a649ce503f9fc06c2f3ecd9d
lianmakesthings <hi@lianmakesthings.dev>
2024-12-13 23:34:09 +0100

    day 3 part 1
    Signed-off-by: lianmakesthings <hi@lianmakesthings.dev>

---
+++ 2024/day3/part1.py
@@ -0,0 +1,11 @@
+import re
+with open('input.txt') as f:
+    lines = f.readlines()
+
+sum = 0
+for line in lines:
+    matches = re.findall("(mul\((\\d{1,3}),(\\d{1,3})\))", line)
+    for m in matches:
+        print(m[1], m[2])
+        sum += int(m[1])*int(m[2])
+print(sum)
